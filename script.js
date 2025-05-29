9af4536acbf43b48417fae1102c2464a382631a0
Jairo Roberto UX - OSF <jairo.roberto@osf.digital>
2025-05-29 18:44:27 -0300

    Js Update

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// <a href="#" onclick="location.reload(); return false;">atualizando sua página</a>
 
-const linkReload = document.getElementById('updatePage');
+// const linkReload = document.getElementById('updatePage');
+// const linkReloadImg = document.getElementById('updatePageImg');
 
-linkReload.addEventListener('click', (event) => {
-  event.preventDefault();
-  location.reload();
+
+// linkReload.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   location.reload();
+// });
+// linkReloadImg.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   location.reload();
+// });
+
+
+const elementsToReload = [document.getElementById('updatePage'), document.getElementById('updatePageImg')];
+
+elementsToReload.forEach(element => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    location.reload();
+  });
 });
