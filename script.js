48a22afeccd4c4271fb451da73f56f9f6ae0679d
Jairo Roberto UX - OSF <jairo.roberto@osf.digital>
2025-05-29 18:48:27 -0300

    Update 2 JS

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
-// const linkReload = document.getElementById('updatePage');
-// const linkReloadImg = document.getElementById('updatePageImg');
+const linkReload = document.getElementById('updatePage');
+const linkReloadImg = document.getElementById('updatePageImg');
 
 
-// linkReload.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   location.reload();
-// });
-// linkReloadImg.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   location.reload();
-// });
-
-
-const elementsToReload = [document.getElementById('updatePage'), document.getElementById('updatePageImg')];
-
-elementsToReload.forEach(element => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    location.reload();
-  });
+linkReload.addEventListener('click', (event) => {
+  event.preventDefault();
+  location.reload();
 });
+linkReloadImg.addEventListener('click', (event) => {
+  event.preventDefault();
+  location.reload();
+});
+
+
+// const elementsToReload = [document.getElementById('updatePage'), document.getElementById('updatePageImg')];
+
+// elementsToReload.forEach(element => {
+//   element.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     location.reload();
+//   });
+// });
