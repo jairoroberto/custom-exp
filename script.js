98ac7811b57b11f9e3e187cc4171e065cfe5ecdf
Jairo Roberto UX - OSF <jairo.roberto@osf.digital>
2025-08-07 14:24:00 -0300

    `Added event listener to update link on click and prevent default behavior`

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('External Script JS');
+
+const updateLink = document.querySelector('.subheader-listview a');
+// reload when click
+// updateLink.href = 'https://example.com/new-link';
+
+updateLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  // do something
+  window.location.href = updateLink.href;
+});
